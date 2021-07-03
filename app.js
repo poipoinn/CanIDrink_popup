62e7b04878e9a3025e24b844a98794cb57435f5c
poipoinn <85345914+poipoinn@users.noreply.github.com>
2021-07-03 18:03:57 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ if (isNaN(age) || age < 0) {    //true일 경우 age는 숫자가 아님
     console.log("You are too young to drink somaek :( ");
 } else if (age >= 20 && age < 55) {
     console.log("You can drink somaek!!! Enjoy it :D");
+} else if ( age === 77) {  // X===Y X가 Y라면
+    console.log("You are very lucky!! 777");
 } else {
     console.log("You'd better not to drink somaek. Let's drink tea for your healthy life.")
 }
